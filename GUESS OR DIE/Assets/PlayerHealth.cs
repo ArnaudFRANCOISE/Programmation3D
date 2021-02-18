5fd2579f46d114ac8ef8910389c6bb5089b6da9a
@ <arnaud.francoise@astudent.yncrea.fr>
2021-02-18 13:12:13 +0100

    guess or die

---
+++ GUESS OR DIE/Assets/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Health= "+ health.ToString());
+        
+    }
+}
